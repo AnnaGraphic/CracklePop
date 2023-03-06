b065de671d2bc5850f54cf8c1394b92147243390
panda <p4nd4@posteo.de>
2023-03-06 18:34:13 +0100

    fix logic: put && on top

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function CracklePop() {
     for (let i = 1; i <= 100; i++){
-        if (i % 3 === 0) {
+        if (i % 5 === 0 && i % 3 === 0) {
             console.log("Crackle")
         } else if (i % 5 === 0) {
             console.log("Pop")
-        } else if (i % 5 === 0 && i % 3 === 0) {
+        } else if (i % 3 === 0) {
             console.log("CracklePop")
         } else {
             console.log(i)
